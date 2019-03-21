69a9d5e516e0a41d5312ddd9601349a828c7d3dc
katfastovets <tuuunes@gmail.com>
2019-03-21 18:12:37 +0200

    refactoring

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../AppHeader';
 import SearchPanel from '../SearchPanel';
@@ -7,23 +7,41 @@ import ItemStatusFilter from '../ItemStatusFilter';
 
 import './App.css';
 
-const todoData = [
-	{label: 'One1', id: 1},
-	{label: 'Two2', id: 2},
-	{label: 'Third3', id: 3}
-];
 
-const App = () => {
-	return (
-		<div className="todo-app">
-			<AppHeader toDo={1} done={3} />
-			<div className="top-panel d-flex">
-				<SearchPanel />
-				<ItemStatusFilter />
+export default class App extends Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			todoData: [
+				{label: 'One1', id: 1},
+				{label: 'Two2', id: 2},
+				{label: 'Third3', id: 3}
+			]
+		};
+
+	}
+
+	deleteItem = (id) => {
+		this.setState(({todoData}) => {
+			const idx = todoData.findIndex((el) => el.id === id);
+			console.log(idx);
+		})
+	};
+
+	render() {
+		return (
+			<div className="todo-app">
+				<AppHeader toDo={1} done={3} />
+				<div className="top-panel d-flex">
+					<SearchPanel />
+					<ItemStatusFilter />
+				</div>
+				<TodoList 
+					todos={this.state.todoData} 
+					onDeleted={this.deleteItem} />
 			</div>
-			<TodoList todos={todoData} />
-		</div>
-	);
+		);
+	}
 };
-
-export default App;
